5b41e1039f5d9dd3881f2d036bacfc28578f53a5
Chris Eastwood <clipse2g@gmail.com>
2018-01-07 22:50:16 -0500

    general tests that don't belong somewhere else

---
+++ QueryBuilder.SqlServer.Tests/Execution/GeneralTests.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace QueryBuilder.SqlServer.Tests.Execution
+{
+    public class GeneralTests : SqlServerDatabaseTest
+    {
+        public GeneralTests(SqlServerDatabaseFixture database) : base(database)
+        {
+
+        }
+    }
+}
