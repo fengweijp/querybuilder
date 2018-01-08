27223e4a4e762e8797faa9652e9eabcceea866ea
Chris Eastwood <clipse2g@gmail.com>
2018-01-07 21:14:49 -0500

    simple connection testing

--- QueryBuilder.SqlServer.Tests/SqlServerDatabaseFixture.cs
+++ QueryBuilder.SqlServer.Tests/SqlServerDatabaseFixture.cs
@@ -3,47 +3,56 @@
  using System.Data.Common;
  using System.Data.SqlClient;
  using System.Linq;
+ using Dapper;
  using SqlKata.Tests;
  using SqlKata.Tests.Execution;
 
 public class SqlServerDatabaseFixture : IExecutionDatabaseFixture
 {
-        private readonly HashSet<WeakReference<SqlConnection>> _tracked = new HashSet<WeakReference<SqlConnection>>();
+    private readonly HashSet<WeakReference<SqlConnection>> _tracked = new HashSet<WeakReference<SqlConnection>>();
 
-        private static SqlConnection GetConnection()
+    private static SqlConnection GetConnection()
+    {
+        var connectionString =
+            QueryBuilderTestSettings.ConnectionStrings.Single(s => s.Name == "sqlserver").ConnectionString;
+        return new SqlConnection(connectionString);
+    }
+
+    public SqlServerDatabaseFixture()
+    {
+        Connection = GetConnection();
+        TestConnection();
+    }
+
+    private void TestConnection()
+    {
+        Connection.ExecuteScalar("SELECT 1");
+    }
+
+    public DbConnection Connection { get; private set; }
+
+    public DbConnection IsolatedConnection()
+    {
+        var connection = GetConnection();
+        _tracked.Add(new WeakReference<SqlConnection>(connection));
+        return connection;
+    }
+
+    public void Dispose()
+    {
+        Connection.Dispose();
+        foreach (var weakReference in _tracked)
         {
-            var connectionString =
-                QueryBuilderTestSettings.ConnectionStrings.Single(s => s.Name == "sqlserver").ConnectionString;
-            return new SqlConnection(connectionString);
-        }
-
-        public SqlServerDatabaseFixture()
-        {
-            Connection = GetConnection();
-        }
-
-        public DbConnection Connection { get; private set; }
-
-        public DbConnection IsolatedConnection()
-        {
-            var connection = GetConnection();
-            _tracked.Add(new WeakReference<SqlConnection>(connection));
-            return connection;
-        }
-
-        public void Dispose()
-        {
-            Connection.Dispose();
-            foreach (var weakReference in _tracked)
+            try
             {
-                try
+                if (weakReference.TryGetTarget(out SqlConnection cnn))
                 {
-                    if (weakReference.TryGetTarget(out SqlConnection cnn))
-                    {
-                        cnn.Dispose();
-                    }
+                    cnn.Dispose();
                 }
-                catch { }
+            }
+            catch
+            {
             }
         }
     }
+}
